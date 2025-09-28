62ca579b8ae73da24c0d290c9d24c39d00b4a16d
Admin <trongtincamerayi@gmail.com>
2025-09-28 21:25:42 +0700

    Thêm tựa đề

--- menu/GiaoDien.Designer.cs
+++ menu/GiaoDien.Designer.cs
@@ -28,19 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(169, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(573, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HỆ THỐNG QUẢN LÝ PHỤ TÙNG XE MÁY";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // GiaoDien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(871, 561);
+            this.Controls.Add(this.label1);
             this.Name = "GiaoDien";
             this.Text = "GiaoDien";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
--- menu/GiaoDien.cs
+++ menu/GiaoDien.cs
@@ -16,5 +16,10 @@ namespace menu
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
