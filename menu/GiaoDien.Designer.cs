32e14376a2be94137c063d0effbd52147825edbd
Ngo Duy Tien <Ngo Duy Tien@PC-DZITI3N>
2025-09-28 21:59:50 +0700

    thêm nút

--- menu/GiaoDien.Designer.cs
+++ menu/GiaoDien.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -42,11 +43,21 @@
             this.label1.Text = "HỆ THỐNG QUẢN LÝ PHỤ TÙNG XE MÁY";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // GiaoDien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(871, 561);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "GiaoDien";
             this.Text = "GiaoDien";
@@ -58,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
