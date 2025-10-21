73f770e7c19dc3c41eb1fe47977d3abf2f10e16d
Tienz123 <nguyenminhtien0880@gmail.com>
2025-10-21 21:12:42 +0700

    thêm frmXuatHoaDon

---
+++ menu/frmXuatHoaDon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace menu
+{
+    public partial class frmXuatHoaDon : Form
+    {
+        public frmXuatHoaDon()
+        {
+            InitializeComponent();
+        }
+
+        private void textEdit1_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
