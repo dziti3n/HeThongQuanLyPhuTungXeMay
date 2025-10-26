f1a6f74a901dfca47020a84048ae42622441753e
Admin <trongtincamerayi@gmail.com>
2025-10-26 14:19:02 +0700

    Hoàn thiện LichSuMuaHang

--- menu/frmLichSuMuaHang.Designer.cs
+++ menu/frmLichSuMuaHang.Designer.cs
@@ -28,36 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvLichSuMuaHang = new System.Windows.Forms.DataGridView();
             this.sodienthoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tenkhachhang = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tenphutung = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.giatien = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtTimKiemTheoSDT = new System.Windows.Forms.TextBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.txtTongTien = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLichSuMuaHang)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvLichSuMuaHang
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvLichSuMuaHang.AllowUserToAddRows = false;
+            this.dgvLichSuMuaHang.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvLichSuMuaHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLichSuMuaHang.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.sodienthoai,
             this.tenkhachhang,
             this.tenphutung,
             this.giatien});
-            this.dataGridView1.Location = new System.Drawing.Point(44, 159);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(651, 327);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvLichSuMuaHang.Location = new System.Drawing.Point(44, 159);
+            this.dgvLichSuMuaHang.Name = "dgvLichSuMuaHang";
+            this.dgvLichSuMuaHang.RowHeadersWidth = 51;
+            this.dgvLichSuMuaHang.RowTemplate.Height = 24;
+            this.dgvLichSuMuaHang.Size = new System.Drawing.Size(651, 327);
+            this.dgvLichSuMuaHang.TabIndex = 0;
+            this.dgvLichSuMuaHang.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLichSuMuaHang_CellClick);
             // 
             // sodienthoai
             // 
@@ -93,23 +94,25 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Tìm kiếm theo số điện thoại:";
             // 
-            // textBox1
+            // txtTimKiemTheoSDT
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(445, 52);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(244, 30);
-            this.textBox1.TabIndex = 2;
+            this.txtTimKiemTheoSDT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimKiemTheoSDT.Location = new System.Drawing.Point(445, 52);
+            this.txtTimKiemTheoSDT.Name = "txtTimKiemTheoSDT";
+            this.txtTimKiemTheoSDT.Size = new System.Drawing.Size(244, 30);
+            this.txtTimKiemTheoSDT.TabIndex = 2;
+            this.txtTimKiemTheoSDT.TextChanged += new System.EventHandler(this.txtTimKiemTheoSDT_TextChanged);
             // 
-            // button1
+            // btnTimKiem
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(594, 88);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 35);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Tìm kiếm";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimKiem.Location = new System.Drawing.Point(594, 88);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(95, 35);
+            this.btnTimKiem.TabIndex = 3;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
             // 
             // label2
             // 
@@ -132,29 +135,30 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Tổng tiền:";
             // 
-            // textBox2
+            // txtTongTien
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(546, 501);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(149, 30);
-            this.textBox2.TabIndex = 6;
+            this.txtTongTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTongTien.Location = new System.Drawing.Point(546, 501);
+            this.txtTongTien.Name = "txtTongTien";
+            this.txtTongTien.Size = new System.Drawing.Size(149, 30);
+            this.txtTongTien.TabIndex = 6;
             // 
             // frmLichSuMuaHang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(740, 561);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtTongTien);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.txtTimKiemTheoSDT);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvLichSuMuaHang);
             this.Name = "frmLichSuMuaHang";
             this.Text = "frmLichSuMuaHang";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.frmLichSuMuaHang_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLichSuMuaHang)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,16 +166,16 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvLichSuMuaHang;
         private System.Windows.Forms.DataGridViewTextBoxColumn sodienthoai;
         private System.Windows.Forms.DataGridViewTextBoxColumn tenkhachhang;
         private System.Windows.Forms.DataGridViewTextBoxColumn tenphutung;
         private System.Windows.Forms.DataGridViewTextBoxColumn giatien;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTimKiemTheoSDT;
+        private System.Windows.Forms.Button btnTimKiem;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtTongTien;
     }
 }
