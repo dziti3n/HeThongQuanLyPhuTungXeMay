7e6ca793ee1031fe1485c93c5f9a461eed5d4f62
Admin <trongtincamerayi@gmail.com>
2025-10-26 12:55:01 +0700

    Thêm lại DonDatHang và ChiTietDonDatHang

--- DoAnMonHoc.DAL/Model/ChiTietDonDatHang.cs
+++ DoAnMonHoc.DAL/Model/ChiTietDonDatHang.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DoAnMonHoc.DAL.Model
 {
-    internal class ChiTietDonDatHang
+    [Table("ChiTietDonDatHang")]
+    public class ChiTietDonDatHang
     {
+        [Key, Column(Order = 0)]
+        [StringLength(10)]
+        public string MaDDH { get; set; }
+
+        [Key, Column(Order = 1)]
+        [StringLength(5)]
+        public string MaPT { get; set; }
+
+        public int? SoLuong { get; set; }
+
+        public decimal? DonGiaNhap { get; set; }
+
+        public virtual DonDatHang DonDatHang { get; set; }
+
+        public virtual PhuTung PhuTung { get; set; }
     }
 }
--- DoAnMonHoc.DAL/Model/DonDatHang.cs
+++ DoAnMonHoc.DAL/Model/DonDatHang.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DoAnMonHoc.DAL.Model
 {
-    internal class DonDatHang
+    [Table("DonDatHang")]
+    public class DonDatHang
     {
+        public DonDatHang()
+        {
+            ChiTietDonDatHangs = new HashSet<ChiTietDonDatHang>();
+        }
+
+        [Key]
+        [StringLength(10)]
+        public string MaDDH { get; set; }
+
+        [StringLength(5)]
+        public string MaNCC { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? NgayDat { get; set; }
+
+        public virtual NhaCungCap NhaCungCap { get; set; }
+
+        public virtual ICollection<ChiTietDonDatHang> ChiTietDonDatHangs { get; set; }
     }
 }
