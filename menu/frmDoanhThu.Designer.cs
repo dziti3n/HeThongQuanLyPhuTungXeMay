7a29b7d9bbeb98ee24b565b80455689e009a2372
USKitatsu <thai48984@gmail.com>
2025-11-03 23:03:05 +0700

    lời vcl

---
+++ menu/frmDoanhThu.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace menu
+{
+    partial class frmDoanhThu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.ThongKeDoanhThu = new menu.ThongKeDoanhThu();
+            this.DoanhThuBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ThongKeDoanhThu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DoanhThuBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.DoanhThuBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "menu.ThongKeDoanhThu.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // ThongKeDoanhThu
+            // 
+            this.ThongKeDoanhThu.DataSetName = "ThongKeDoanhThu";
+            this.ThongKeDoanhThu.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // DoanhThuBindingSource
+            // 
+            this.DoanhThuBindingSource.DataMember = "DoanhThu";
+            this.DoanhThuBindingSource.DataSource = this.ThongKeDoanhThu;
+            // 
+            // frmDoanhThu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "frmDoanhThu";
+            this.Text = "frmDoanhThu";
+            this.Load += new System.EventHandler(this.frmDoanhThu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ThongKeDoanhThu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DoanhThuBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource DoanhThuBindingSource;
+        private ThongKeDoanhThu ThongKeDoanhThu;
+    }
+}
